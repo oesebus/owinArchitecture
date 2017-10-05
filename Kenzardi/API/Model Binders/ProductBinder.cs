0719cb691651b48061aaf408e91adcd192d9f659
issou <idrissisolutions@gmail.com>
2017-10-05 20:48:09 +0200

    * Kenzardi.userprefs: 	  Add model binder
    	* Kenzardi.API.csproj:
    	* ProductBinder.cs:

---
+++ Kenzardi/API/Model Binders/ProductBinder.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Web.Http.Controllers;
+using System.Web.Http.ModelBinding;
+
+namespace ModelBinders
+{
+	public class ProductBinder : IModelBinder
+	{
+		public ProductBinder()
+		{
+
+		}
+
+		public bool BindModel(HttpActionContext actionContext, ModelBindingContext bindingContext)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
