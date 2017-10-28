d73af66c9eaf3afd482e7804242cdc4805087706
issou <idrissisolutions@gmail.com>
2017-10-28 02:16:07 +0200

    pattern state for customer model

---
+++ Kenzardi.Core/Customers/Model/AbstractCustomer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Customers.Model
+{
+	public class AbstractCustomer
+	{
+		protected double discountAmount;
+		protected double PerProductOrderLimit;
+		protected Boolean CanCancelOrder;
+		protected Boolean CanEditOrder;
+
+		public AbstractCustomer()
+		{
+
+		}
+	}
+}
